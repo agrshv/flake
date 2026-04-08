1d690bf6917aa4bba5c2ab0be783f772abb1e8c0
d3spair <git@agrshv.dev>
2026-04-08 09:44:46 +0500

    add podman

--- home.nix
+++ home.nix
@@ -17,6 +17,7 @@
     ./programs/mako.nix
     ./programs/nh.nix
     ./programs/obsidian.nix
+    ./programs/podman.nix
     ./programs/remmina.nix
     ./programs/ssh.nix
     ./programs/starship.nix
