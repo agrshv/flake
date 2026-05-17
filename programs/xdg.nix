c19527ef88c432c46d5b71fa149a4fac7fc7e0a5
d3spair <git@agrshv.dev>
2026-05-17 22:36:35 +0500

    update xdg

--- programs/xdg.nix
+++ programs/xdg.nix
@@ -1,3 +1,35 @@
+{ config, ... }:
 {
-  xdg.autostart.enable = true;
+  xdg = {
+    autostart.enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+
+      download = "${config.home.homeDirectory}/downloads";
+      desktop = "${config.home.homeDirectory}/desktop";
+      documents = "${config.home.homeDirectory}/documents";
+
+      publicShare = "${config.home.homeDirectory}/.local/public";
+      templates = "${config.home.homeDirectory}/.local/templates";
+
+      music = "${config.home.homeDirectory}/media/music";
+      pictures = "${config.home.homeDirectory}/media/pictures";
+      videos = "${config.home.homeDirectory}/media/videos";
+    };
+    mimeApps = {
+      enable = true;
+      #TODO research into xdg.mimeApps.defaultApplicationPackages
+      defaultApplications = {
+        "text/html" = [ "brave-browser.desktop" ]; # or com.brave.Browser.desktop
+        "x-scheme-handler/discord" = [ "vesktop.desktop" ];
+        "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+        "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop" ];
+      };
+      associations.added = {
+        "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+        "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop" ];
+      };
+    };
+  };
 }
